f3111f09db1f35b6ca66f61684e3715bfa89e460
GKPedrsen <GKPedrsen@gmail.com>
2025-01-06 13:45:53 +0100

    ValiderPersonnummer: initial

---
+++ ValiderPersonnummer/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Text.Unicode;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ValiderPersonnummer;
+
+public partial class MainWindow : Window
+{
+  public MainWindow()
+  {
+    InitializeComponent();
+  }
+
+  private void InputÆndret(object sender, RoutedEventArgs e)
+  {
+    SvarLabel.Content = "";
+  }
+
+  private void Valider_Click(object sender, RoutedEventArgs e)
+  {
+    if(Validator.Validate(Input.Text))
+    {
+      SvarLabel.Content = "GYLDIGT";
+      SvarLabel.Foreground = new SolidColorBrush(Colors.LawnGreen);
+      return;
+    }
+
+    SvarLabel.Content = "IKKE GYLDIGT";
+
+    SvarLabel.Foreground = new SolidColorBrush(Colors.Red);
+  }
+}
---
+++ ValiderPersonnummer/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValiderPersonnummer
+{
+  static class Validator
+  {
+    public static bool Validate(string cpr)
+    {
+      //kontroller at cpr stringen har en længde på 10 (antal kontrol vægte) og at de alle er numeriske tal
+      if (cpr.Length != kontrolVægte.Length || cpr.Any(c => c < '0' || c > '9'))
+        return false;
+
+      //konverter fra text til talværdi
+      int[] ciffere = cpr.Select(c => (int)(c - '0')).ToArray();
+
+      //gang hver ciffer med dets korresponderende kontrol vægt og summer det hele
+      int vægtetSum = ciffere.Zip(kontrolVægte, (ciffer, vægt) => ciffer*vægt).Sum();
+
+      //personnummeret er validt hvis det går op med 11
+      return (vægtetSum % 11) == 0;
+    }
+
+    private static int[] kontrolVægte = { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1};
+  }
+}
